71de465a77387441cd1128a62987b81ff355a4c3
Deonte Kilgore <deontecodes@gmail.com>
2019-10-27 17:14:12 -0400

    Added new feature. When user begins typing the list filters results based on the letters typed in, while also showing you how many results it's filtering out of the whole list.

--- Pokemon/Controller/PokemonController.swift
+++ Pokemon/Controller/PokemonController.swift
@@ -17,18 +17,21 @@ var didYouGetPokemon = false
 class PokemonController: UITableViewController, UISearchBarDelegate, UISearchResultsUpdating {
     
     //MARK:- Instance Variables
-//    var searchFooterBottomConstraint: CGFloat = 0
-//
-//    lazy var searchFooter: SearchFooter = {
-//        let sf = SearchFooter()
-//        sf.translatesAutoresizingMaskIntoConstraints = false
-//        sf.addConstraint(NSLayoutConstraint(item: sf, attribute: .bottom, relatedBy: .equal, toItem: self.view, attribute: .bottom, multiplier: 1, constant: searchFooterBottomConstraint))
-//        sf.addConstraint(NSLayoutConstraint(item: sf, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1, constant: 0))
-//        sf.addConstraint(NSLayoutConstraint(item: sf, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1, constant: 0))
-//        sf.addConstraint(NSLayoutConstraint(item: sf, attribute: .height, relatedBy: .equal, toItem: self, attribute: .height, multiplier: 0, constant: 44))
-//        return sf
-//    }()
-//
+    lazy var searchFooterBottomConstraint = tabBarController?.tabBar.frame.minY//tabBarController?.tabBar.frame.origin.y ?? 0
+    
+    var keyboardHeightVar: CGFloat! {
+        didSet {
+            searchFooter.frame = CGRect(x: 0, y: searchFooterBottomConstraint! - self.keyboardHeightVar, width: 414, height: 44)
+        }
+    }
+  
+    fileprivate var searchFooter: SearchFooter  = {
+        let view = SearchFooter()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+
     fileprivate let pokemonHud = JGProgressHUD(style: .dark)
     fileprivate let searchController = UISearchController(searchResultsController: nil)
     fileprivate let randomOffset = 0 //Int.random(in: 0 ..< 914)
@@ -43,15 +46,27 @@ class PokemonController: UITableViewController, UISearchBarDelegate, UISearchRes
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .mainBackgroundColor
-        //view.addSubview(searchFooter)
+    
         setupTableView()
         setupSearchController()
         checkPokemonData()
-        //setupNotifications()
+        setupNotifications()
         
     }
     
+    override func viewDidLayoutSubviews() {
+        setupFooter()
+    }
 
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+          keyboardHeightVar = 0
+          searchFooter.frame = CGRect(x: 0, y: searchFooterBottomConstraint! - self.keyboardHeightVar, width: 414, height: 44)
+    }
+
+    fileprivate func setupFooter() {
+        navigationController!.view.addSubview(searchFooter)
+    }
     
     //MARK:- Helper Functions
     
@@ -75,39 +90,38 @@ class PokemonController: UITableViewController, UISearchBarDelegate, UISearchRes
           self.pokemon = sortedPokemon
       }
     
-//    fileprivate func setupNotifications() {
-//        let notificationCenter = NotificationCenter.default
-//        notificationCenter.addObserver(forName: UIResponder.keyboardWillChangeFrameNotification, object: nil, queue: .main) { (notification) in
-//                                        self.handleKeyboard(notification: notification)
-//
-//        }
-//        notificationCenter.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { (notification) in
-//                                        self.handleKeyboard(notification: notification)
-//        }
-//    }
+    fileprivate func setupNotifications() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(forName: UIResponder.keyboardWillChangeFrameNotification, object: nil, queue: .main) { (notification) in
+                                        self.handleKeyboard(notification: notification)
+
+        }
+        notificationCenter.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { (notification) in
+                                        self.handleKeyboard(notification: notification)
+        }
+    }
     
-//    func handleKeyboard(notification: Notification) {
-//        // 1
-//        guard notification.name == UIResponder.keyboardWillChangeFrameNotification else {
-//            searchFooterBottomConstraint = 0
-//            view.layoutIfNeeded()
-//            return
-//        }
-//
-//        guard
-//            let info = notification.userInfo,
-//            let keyboardFrame = info[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue
-//            else {
-//                return
-//        }
-//
-//        // 2
-//        let keyboardHeight = keyboardFrame.cgRectValue.size.height
-//        UIView.animate(withDuration: 0.1, animations: { () -> Void in
-//            self.searchFooterBottomConstraint = keyboardHeight
-//            self.view.layoutIfNeeded()
-//        })
-//    }
+    func handleKeyboard(notification: Notification) {
+        // 1
+        guard notification.name == UIResponder.keyboardWillChangeFrameNotification else {
+            keyboardHeightVar = 0
+            print("Keyboard will change")
+            view.layoutIfNeeded()
+            return
+        }
+        
+        guard let info = notification.userInfo, let keyboardFrame = info[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+        // 2
+        let originYval = keyboardFrame.cgRectValue.origin.y
+        let keyboardHeight = keyboardFrame.cgRectValue.size.height
+        UIView.animate(withDuration: 0.1, animations: { () -> Void in
+            self.keyboardHeightVar = keyboardHeight
+            self.searchFooter.frame = CGRect(x: 0, y: originYval - 44/*self.searchFooterBottomConstraint! - self.keyboardHeightVar*/, width: 414, height: 44)
+            
+            self.view.layoutIfNeeded()
+        })
+    }
     
     //MARK:- Setup Search
     
@@ -115,32 +129,14 @@ class PokemonController: UITableViewController, UISearchBarDelegate, UISearchRes
         navigationItem.searchController = searchController
         navigationItem.hidesSearchBarWhenScrolling = false
         searchController.obscuresBackgroundDuringPresentation = false
-        //searchController.searchBar.delegate = self
-        
+     
         searchController.searchResultsUpdater = self
         searchController.searchBar.placeholder = "Search Pokemon"
         definesPresentationContext = true
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        print(searchText)
-        
-        //let pokeURL = "https://pokeapi.co/api/v2/pokemon/\(searchText.lowercased())/"
-        
-        pokemon.forEach { (pokemon) in
-            
-//            if pokemon.name.contains(searchText.lowercased()) == true {
-//                print("Its true!")
-//                filteredPokemon.append(pokemon)
-//                filteredPokemon.forEach { (pokemon) in
-//                    print(pokemon.name)
-//                }
-//            } else if searchText.isEmpty || searchText == "" {
-//                filteredPokemon.removeAll()
-//                print(filteredPokemon.isEmpty)
-//            }
-        }
-        
+
     }
     
     func updateSearchResults(for searchController: UISearchController) {
@@ -173,19 +169,16 @@ class PokemonController: UITableViewController, UISearchBarDelegate, UISearchRes
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if isFiltering {
-           // searchFooter.setIsFilteringToShow(filteredItemCount: filteredPokemon.count, of: pokemon.count)
+            searchFooter.setIsFilteringToShow(filteredItemCount: filteredPokemon.count, of: pokemon.count)
             return filteredPokemon.count
         }
-        
+        searchFooter.setNotFiltering()
         return pokemon.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: pokemonCellID, for: indexPath) as! PokemonCell
-        
-//        let pokemon = self.pokemon[indexPath.row]
-//        cell.pokemon = pokemon
-        
+
         let pokemon: Pokemon
         
         if isFiltering {
@@ -279,11 +272,3 @@ class PokemonController: UITableViewController, UISearchBarDelegate, UISearchRes
     }
     
 }
-
-
-
-//
-//let sortedPokemon = pokemon.sorted { (id0, id1 ) -> Bool in
-//                       return id0.id < id1.id
-//                   }
-
